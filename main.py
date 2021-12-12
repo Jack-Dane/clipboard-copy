0017768a2d62c39ec54c004d1cb132837d296bdd
Jack Dane <jackd98.jd@gmail.com>
2021-12-12 14:14:16 +0000

    Allowing nice exit and setup tools

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from Models import ClipboardPoller, QueueObject
 from Views import Application
 from Controllers import Controller
@@ -6,13 +8,21 @@ from Controllers import Controller
 def main():
     clipboardQueue = QueueObject()
     clipboardPoller = ClipboardPoller(clipboardQueue)
+    clipboardPoller.daemon = True
     clipboardPoller.start()
 
     controller = Controller(clipboardQueue)
     controller.attachModel(clipboardPoller)
     controller.attachView(Application)
+    controller.daemon = True
     controller.start()
+
+    clipboardPoller.join()
+    controller.join()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit("Terminating Clipboard-Copy Application")
